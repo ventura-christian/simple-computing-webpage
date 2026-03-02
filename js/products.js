a765809b9021c28f497b46a4d9f9e9250244dfc9
christianVentura <119697304+ventura-christian@users.noreply.github.com>
2026-03-01 20:36:35 -0800

    What I did: Tested the immediately invoked function expression by breaking the structure. I left it in for myself as a reminder on how it could break.

--- js/products.js
+++ js/products.js
@@ -1,3 +1,4 @@
+// Properly implemented IIFE
 (function () {
   "use strict";
 
@@ -7,3 +8,16 @@
 
   window.initProducts = initProducts;
 })();
+
+
+// Intentionally broken IIFE to see what the outcome is when I attempt to load the page.
+// function ()
+// ( {
+// "use strict";
+
+//   function initProducts() {
+//     console.log("Products module initialized");
+//   }
+
+//   window.initProducts = initProducts;
+// })();
